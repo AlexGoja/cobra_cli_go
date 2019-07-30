d551fdb93531341bc636b763bdd08f8526eb46b8
Alexandru Goja <Alexandru.Goja@thomsonretuers.com>
2019-07-30 01:02:08 +0100

    add comments to relevant parts

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 
+	// Creates the devops-tool root command
 	root := cmd.CreateRootCommand()
+
+	// Creates aws sessions
 	sess := aws.CreateAWSSession()
 
+	//Instantiate clients
 	aws.CreateDevToolVersion(root, sess)
 	aws.CreateEC2(root, sess)
 	aws.CreateCloudformation(root, sess)
